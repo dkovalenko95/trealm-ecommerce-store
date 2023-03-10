c13a3a4185bef72aa3f649a3ddbae2effa2877f9
Dmytro <dkovalenko2425@gmail.com>
2023-03-10 15:19:53 +0200

    components  set up made

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,22 @@
 import React from 'react';
 
+import { HeroBanner, Product, FooterBanner } from './../components/index';
+
 const Home = () => {
   return (
     <>
-      HeroBanner
+      <HeroBanner />
 
-      <div>
+      <div className='products-heading'>
         <h2>Best Selling Products</h2>
         <p>A large selection of high quality headphones</p>
       </div>
 
-      <div>
+      <div className='products-container'>
         {['Prod 1', 'Prod 2', 'Prod 3'].map((product) => product)}
       </div>
 
-      Footer
+      <FooterBanner />
     </>
   );
 };
