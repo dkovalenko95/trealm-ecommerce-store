3355060a8b7ead84e57d0d42365b4f3f2fad750d
Dmytro <dkovalenko2425@gmail.com>
2023-03-10 13:18:45 +0200

    product.js schema for sanity created

---
+++ sanity_ecommerce_store/schemas/product.js
@@ -0,0 +1,50 @@
+export default {
+  name: 'product',
+  title: 'Product',
+  type: 'document',
+  fields: [
+
+    // Img
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'array',
+      of: [{ type: 'image' }],
+      options: {
+        hotspot: true,
+      }
+    },
+
+    // Name
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+
+    // Unique URL/string
+    {
+      slug: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 90,
+      }
+    },
+
+    // Price
+    {
+      name: 'price',
+      title: 'Price',
+      type: 'number',
+    },
+
+    // Details
+    {
+      name: 'details',
+      title: 'Details',
+      type: 'string',
+    }
+  ]
+};
