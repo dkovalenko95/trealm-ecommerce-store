0b14d30f9bd08d8c4a8cd417de63f2178d3ee719
Dmytro <dkovalenko2425@gmail.com>
2023-03-21 11:11:17 +0200

    shipping options in stripe.js fixed

--- src/pages/api/stripe.js
+++ src/pages/api/stripe.js
@@ -27,9 +27,11 @@ export default async function handler(req, res) {
                 maximum: {unit: 'business_day', value: 7},
               },
             },
+          },
+          {
             shipping_rate_data: {
               type: 'fixed_amount',
-              fixed_amount: {amount: 15, currency: 'usd'},
+              fixed_amount: {amount: 1500, currency: 'usd'},
               display_name: 'Fast shipping',
               delivery_estimate: {
                 minimum: {unit: 'business_day', value: 1},
